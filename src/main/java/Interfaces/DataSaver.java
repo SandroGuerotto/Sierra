02a59909aa07cde4fb503ba60a99c7af78a0dea4
Burim Cakolli <burim.cakolli@hotmail.com>
2017-01-10 10:27:16 +0100

    Added Class Person & Roll

--- src/main/java/Interfaces/DataSaver.java
+++ src/main/java/Interfaces/DataSaver.java
@@ -14,6 +14,7 @@ package Interfaces;
 import data.Absent;
 import data.Gesuch;
 import data.Mark;
+import data.Person;
 import data.Request;
 import data.Schoolclass;
 import javafx.collections.ObservableList;
@@ -33,7 +34,7 @@ public interface DataSaver {
 	//-class_Request
 	public Integer insert_Request(Request request);
 	public void update_Request(Request request);
-	public Object get_Request(int id);
+	public Request get_Request(int id);
 	public ObservableList<Request> getAllRequests(int id_person);
 	public void delete_Request(Request request);
 	//-End Request
@@ -41,7 +42,7 @@ public interface DataSaver {
 	//-class_Gesuch
 	public Integer insert_Gesuch(Gesuch gesuch);
 	public void update_Gesuch(Gesuch gesuch);
-	public Object get_Gesuch(int id);
+	public Gesuch get_Gesuch(int id);
 	public ObservableList<Gesuch> getAllGesuche(int id_person);
 	public void delete_Gesuch(Gesuch gesuch);
 	//-End Gesuch
@@ -49,7 +50,7 @@ public interface DataSaver {
 	//-class_ItemEvent
 	public Integer insert_ItemEvent(ItemEvent event);
 	public void update_ItemEvent(ItemEvent event);
-	public Object get_ItemEvent(int id);
+	public ItemEvent get_ItemEvent(int id);
 	public ObservableList<ItemEvent> getAllEvents();
 	public void delete_ItemEvent(ItemEvent event);
 	//-End ItemEvent	
@@ -57,7 +58,7 @@ public interface DataSaver {
 	//-class_Mark
 	public Integer insert_Mark(Mark mark);
 	public void update_Mark(Mark mark);
-	public Object get_Mark(int id);
+	public Mark get_Mark(int id);
 	public ObservableList<Mark> getAllMarks(int id_person);
 	public void delete_Mark(Mark mark);
 	//-End Mark
@@ -65,26 +66,26 @@ public interface DataSaver {
 	//-class_SchoolClass
 	public Integer insert_Schoolclass(Schoolclass schoolClass);
 	public void update_Schoolclass(Schoolclass schoolClass);
-	public Object get_Schoolclass(int id);
+	public Schoolclass get_Schoolclass(int id);
 	public ObservableList<Schoolclass> getAllSchoolclasses();
 	public void delete_Schoolclass(Schoolclass schoolClass);
 	//-End SchoolClass
 	
 	//-class_ClassMember
-	public Integer insert_ClassMember(ClassMember classMember);
-	public void update_ClassMember(ClassMember classMember);
-	public Object get_ClassMember(int id);
+	public Integer insert_ClassMember(Person person, Schoolclass sclass);
+	public void update_ClassMember(Person person, Schoolclass sclass);
+	public ClassMember get_ClassMember(Person person, Schoolclass sclass);
 	public ObservableList<ClassMember> getAllTeachers();
 	public ObservableList<ClassMember> getAllPeople();	
-	public void delete_ClassMember(ClassMember classMember);
+	public void delete_ClassMember(Person person, Schoolclass sclass);
 	//-End ClassMember
 	
 	//-class_Absent
 	public Integer insert_Absent(Absent absent); 
 	public void update_Absent(Absent absent);
-	public Object get_Absent(int id);
+	public Absent get_Absent(int id);
 	public ObservableList<Absent> getAllAbsents(int id_person);
 	public void delete_Absent(Absent absent);
 	//-End Absent
-		 
+			 
 }//-class
---
+++ src/main/java/data/Person.java
@@ -0,0 +1,103 @@
+/**
+ * @author :   Burim Cakolli
+ * Turns coffee & pizza into Software
+ * Created          :   10.01.2017
+ * Project          :   Sierra
+ * Package          :   data
+ * @version 		:   1.0
+ * LastUpdated      :   10.01.2017 / by Burim Cakolli
+ * Description      :
+ * 
+ */
+package data;
+
+public class Person {
+	private int id;
+	private String name;
+	private String forename;
+	private String birthdate;
+	private String telnr;
+	private String email;
+	private String addText;
+	private Roll rolle;
+	private String password;
+	private String username;
+			
+	public Person(int id, String name, String forename, String birthdate, String telnr, String email, String addText,
+			Roll rolle, String password, String username) {
+		this.id = id;
+		this.name = name;
+		this.forename = forename;
+		this.birthdate = birthdate;
+		this.telnr = telnr;
+		this.email = email;
+		this.addText = addText;
+		this.rolle = rolle;
+		this.password = password;
+		this.username = username;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getForename() {
+		return forename;
+	}
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getTelnr() {
+		return telnr;
+	}
+	public void setTelnr(String telnr) {
+		this.telnr = telnr;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddText() {
+		return addText;
+	}
+	public void setAddText(String addText) {
+		this.addText = addText;
+	}
+	public Roll getRolle() {
+		return rolle;
+	}
+	public void setRolle(Roll rolle) {
+		this.rolle = rolle;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+	
+}//-class
