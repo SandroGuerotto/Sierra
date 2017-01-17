318ed9a1d65ebc8c123871090f2bf6aff304da29
SandroGuerotto <s.guerotto@gmail.com>
2017-01-17 13:04:44 +0100

    Push Notification

--- src/main/java/controller/StarterGui.java
+++ src/main/java/controller/StarterGui.java
@@ -9,7 +9,6 @@ package controller;
  */
 
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
